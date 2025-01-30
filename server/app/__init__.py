bbd0c134b16d73ad2a0f7b8048b634ce063e7e0f
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-30 21:00:38 +0300

    API blueprints has been set in the app method

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -18,4 +18,18 @@ def create_app():
     migrate.init_app(app, db)
     ma.init_app(app)
 
+    from .api.user_api import user_bp
+    from .api.category_api import category_bp
+    from .api.order_api import order_bp
+    from .api.address_api import address_bp
+    from .api.product_api import product_bp
+    from .api.order_detail_api import order_detail_bp
+
+    app.register_blueprint(user_bp, url_prefix="/user")
+    app.register_blueprint(category_bp, url_prefix="/category")
+    app.register_blueprint(order_bp, url_prefix="/order")
+    app.register_blueprint(address_bp, url_prefix="/address")
+    app.register_blueprint(product_bp, url_prefix="/product")
+    app.register_blueprint(order_detail_bp, url_prefix="/order-detail")
+
     return app
