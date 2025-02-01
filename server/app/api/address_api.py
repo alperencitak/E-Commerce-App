acc1f40ce95a9e5e5521ce25b91a402f38e2a1c1
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-01 13:53:11 +0300

    Address api modernized

--- server/app/api/address_api.py
+++ server/app/api/address_api.py
@@ -1,34 +1,46 @@
-from flask import Blueprint, request, jsonify
+from flask_smorest import Blueprint
 from app.service.address_service import AddressService
+from http import HTTPStatus
+from app.model.address import AddressSchema
+from flask.views import MethodView
 
-address_bp = Blueprint("address_api", __name__)
-
-
-@address_bp.route("/id")
-def get_by_id():
-    address_id = request.args.get('id')
-    return jsonify(AddressService.get_by_id(address_id)), 200
+address_bp = Blueprint("addresses", "addresses", url_prefix="/address", description="Address Management API")
 
 
-@address_bp.route("/user")
-def get_by_user_id():
-    user_id = request.args.get('id')
-    return jsonify(AddressService.get_by_user_id(user_id)), 200
+@address_bp.route("/id/<int:address_id>")
+class AddressResponse(MethodView):
+    @address_bp.response(HTTPStatus.OK, AddressSchema)
+    def get(self, address_id):
+        return AddressService.get_by_id(address_id)
 
 
-@address_bp.route("/add", methods=['POST'])
-def add():
-    data = request.get_json()
-    return jsonify(AddressService.add(data)), 201
+@address_bp.route("/user/<int:user_id>")
+class AddressesResponse(MethodView):
+    @address_bp.response(HTTPStatus.OK, AddressSchema(many=True))
+    def get(self, user_id):
+        return AddressService.get_by_user_id(user_id)
 
 
-@address_bp.route("/delete", methods=['DELETE'])
-def delete_by_id():
-    address_id = request.args.get('id')
-    return jsonify(AddressService.delete_by_id(address_id)), 204
+@address_bp.route("/add")
+class AddAddressResponse(MethodView):
+    @address_bp.response(HTTPStatus.CREATED, AddressSchema)
+    @address_bp.arguments(AddressSchema)
+    def post(self, data):
+        print(data)
+        return AddressService.add(data)
 
 
-@address_bp.route("/update", methods=['PUT'])
-def update():
-    data = request.get_json()
-    return jsonify(AddressService.update(data)), 200
+@address_bp.route("/delete/<int:address_id>")
+class DeleteAddressResponse(MethodView):
+    @address_bp.response(HTTPStatus.NO_CONTENT)
+    def delete(self, address_id):
+        AddressService.delete_by_id(address_id)
+        return ""
+
+
+@address_bp.route("/update")
+class UpdateAddressResponse(MethodView):
+    @address_bp.response(HTTPStatus.OK, AddressSchema)
+    @address_bp.arguments(AddressSchema)
+    def put(self, data):
+        return AddressService.update(data)
