9f426511922eae33758def4922349a176deb8fef
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-09 15:17:05 +0300

    Retrofit changed for Test User API

--- client/app/src/main/java/com/alperencitak/e_commerce_app/api/RetrofitInstance.kt
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/api/RetrofitInstance.kt
@@ -6,11 +6,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     private const val BASE_URL = "http://10.0.2.2:5000/"
 
-    val api: BaseApiService by lazy {
+    val api: UserApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(BaseApiService::class.java)
+            .create(UserApiService::class.java)
     }
 }
