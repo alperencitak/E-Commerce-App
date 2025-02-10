13865a2d6d3bbb5de81551b9aab6d74a119d994d
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-10 15:13:44 +0300

    Auth api written

---
+++ server/app/api/auth_api.py
@@ -0,0 +1,27 @@
+from flask.views import MethodView
+from werkzeug.exceptions import NotFound
+from flask import abort
+
+from app.service.auth_service import AuthService
+from app.service.user_service import UserService
+from app.model.user import UserSchema, LoginSchema, RegisterSchema
+from flask_smorest import Blueprint
+from http import HTTPStatus
+
+auth_bp = Blueprint("auths", "auths", url_prefix="/auth", description="Authentication API")
+
+
+@auth_bp.route("/login")
+class LoginResource(MethodView):
+    @auth_bp.response(HTTPStatus.OK, UserSchema)
+    @auth_bp.arguments(LoginSchema)
+    def get(self, data):
+        return AuthService.login(data)
+
+
+@auth_bp.route("/register")
+class RegisterResource(MethodView):
+    @auth_bp.response(HTTPStatus.OK, UserSchema)
+    @auth_bp.arguments(RegisterSchema)
+    def post(self, data):
+        return AuthService.register(data)
