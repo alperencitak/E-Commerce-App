665c7955489d31aa7602d032c1c8d0cecbcc11e4
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-12 17:45:06 +0300

    Update RetrofitInstance.kt

--- client/app/src/main/java/com/alperencitak/e_commerce_app/api/RetrofitInstance.kt
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/api/RetrofitInstance.kt
@@ -6,11 +6,39 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     private const val BASE_URL = "http://10.0.2.2:5000/"
 
-    val api: UserApiService by lazy {
+    private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(UserApiService::class.java)
     }
+
+    val userApi: UserApiService by lazy {
+        retrofit.create(UserApiService::class.java)
+    }
+
+    val addressApi: AddressApiService by lazy {
+        retrofit.create(AddressApiService::class.java)
+    }
+
+    val authApi: AuthApiService by lazy {
+        retrofit.create(AuthApiService::class.java)
+    }
+
+    val categoryApi: CategoryApiService by lazy {
+        retrofit.create(CategoryApiService::class.java)
+    }
+
+    val productApi: ProductApiService by lazy {
+        retrofit.create(ProductApiService::class.java)
+    }
+
+    val orderApi: OrderApiService by lazy {
+        retrofit.create(OrderApiService::class.java)
+    }
+
+    val orderDetailApi: OrderDetailApiService by lazy {
+        retrofit.create(OrderDetailApiService::class.java)
+    }
+
 }
