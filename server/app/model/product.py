b261fe747d8e84a6f58dfdc0f5ac4c8a9cd6431e
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-15 20:04:41 +0300

    İmage url added in product model

--- server/app/model/product.py
+++ server/app/model/product.py
@@ -11,6 +11,7 @@ class Product(db.Model):
     price = db.Column("price", db.Numeric(10, 2), nullable=False)
     stock = db.Column("stock", db.Integer, default=10, nullable=False)
     category_id = db.Column("category_id", db.Integer, db.ForeignKey("categories.category_id"), nullable=False)
+    image_url = db.Column("image_url", db.String(120), nullable=True)
     created_at = db.Column("created_at", db.DateTime, default=db.func.now())
     updated_at = db.Column("updated_at", db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
