18e9fef2fdc584a378e0324efccf662137a1ec39
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-16 17:19:24 +0300

    Login Page just created

--- client/app/src/main/java/com/alperencitak/e_commerce_app/view/LoginPage.kt
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/view/LoginPage.kt
@@ -1,16 +1,26 @@
 package com.alperencitak.e_commerce_app.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.alperencitak.e_commerce_app.viewmodel.UserViewModel
+import androidx.navigation.NavHostController
+import com.alperencitak.e_commerce_app.viewmodel.AuthViewModel
+
 
 @Composable
-fun LoginPage() {
-    val userViewModel: UserViewModel = hiltViewModel()
-    val user = userViewModel.user.collectAsState()
-    userViewModel.fetchUserById(101)
-    user.value?.let {
-        println(it.first_name)
+fun LoginPage(navHostController: NavHostController){
+
+    val authViewModel: AuthViewModel = hiltViewModel()
+    val loginResponse = authViewModel.loginResponse.collectAsState()
+    val registerResponse = authViewModel.registerResponse.collectAsState()
+    val loading = authViewModel.loading.collectAsState()
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
     }
 }
