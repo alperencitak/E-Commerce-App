596ac5b202e743f3baf8ec72db6082755341a4d2
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-16 17:20:03 +0300

    Nav Screen adjusted

---
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/view/NavScreen.kt
@@ -0,0 +1,26 @@
+package com.alperencitak.e_commerce_app.view
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+
+
+@Composable
+fun NavScreen(navController: NavHostController, paddingValues: PaddingValues) {
+    NavHost(
+        navController = navController,
+        startDestination = "main",
+        modifier = Modifier.fillMaxSize()
+    ){
+        composable("main"){
+            HomePage(navController)
+        }
+        composable("login"){
+            LoginPage(navController)
+        }
+    }
+}
