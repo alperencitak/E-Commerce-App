3bbf0112e9f7065f8a198e01b2d9ea57fb79b7c0
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-16 17:17:38 +0300

    Color added

--- client/app/src/main/java/com/alperencitak/e_commerce_app/ui/theme/Color.kt
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val SoftBeige = Color(0xFFB0ACA6)
+val LightGray = Color(0xFFC8C6C3)
+val LightCream = Color(0xFFE2E3E2)
