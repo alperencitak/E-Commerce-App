613ca65d2e372daea0af52287162d6e5c2575b20
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-19 20:34:02 +0300

    Request model fixed

--- client/app/src/main/java/com/alperencitak/e_commerce_app/model/RegisterRequest.kt
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/model/RegisterRequest.kt
@@ -1,8 +1,8 @@
 package com.alperencitak.e_commerce_app.model
 
 data class RegisterRequest(
-    val firstName: String,
-    val lastName: String,
+    val first_name: String,
+    val last_name: String,
     val email: String,
     val phone: String,
     val password: String
