a9487e60ca2f920b02f6644ed740143124a9d7c9
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-24 00:38:40 +0300

    Added parent_id in Category Model

--- server/app/model/category.py
+++ server/app/model/category.py
@@ -6,9 +6,12 @@ class Category(db.Model):
 
     category_id = db.Column("category_id", db.Integer, primary_key=True, autoincrement=True)
     name = db.Column("name", db.String(100), nullable=False)
+    parent_id = db.Column("parent_id", db.Integer, db.ForeignKey("categories.category_id"), nullable=True)
 
 
 class CategorySchema(ma.SQLAlchemyAutoSchema):
+    parent_id = ma.Integer(required=False, allow_none=True)
+    
     class Meta:
         model = Category
         load_instance = True
