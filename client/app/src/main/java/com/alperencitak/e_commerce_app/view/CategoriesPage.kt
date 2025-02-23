3d7765d424656dca455cfb47df78165f4b990f89
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-24 00:40:12 +0300

    Categories Page written

---
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/view/CategoriesPage.kt
@@ -0,0 +1,100 @@
+package com.alperencitak.e_commerce_app.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.alperencitak.e_commerce_app.R
+import com.alperencitak.e_commerce_app.ui.theme.Blue
+import com.alperencitak.e_commerce_app.ui.theme.LightBlue
+import com.alperencitak.e_commerce_app.ui.theme.LightCream
+import com.alperencitak.e_commerce_app.ui.theme.SoftBeige
+import com.alperencitak.e_commerce_app.viewmodel.CategoryViewModel
+
+
+@Composable
+fun CategoriesPage(navHostController: NavHostController) {
+    val categoryViewModel: CategoryViewModel = hiltViewModel()
+    val categories = categoryViewModel.categoryList.collectAsState()
+    val loading = categoryViewModel.loading.collectAsState()
+    categoryViewModel.fetchAllParent()
+    val font = FontFamily(
+        Font(
+            R.font.montserrat_extrabold
+        )
+    )
+    Column(
+        modifier = Modifier.fillMaxSize().background(LightCream)
+    ) {
+        Text(
+            text = "CATEGORIES",
+            fontFamily = font,
+            fontSize = 18.sp,
+            color = SoftBeige,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 64.dp, bottom = 48.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            items(categories.value) { category ->
+                ElevatedCard(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
+                        .padding(12.dp),
+                    shape = RoundedCornerShape(8.dp),
+                    elevation = CardDefaults.cardElevation(4.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.White)
+                    ) {
+                        Text(
+                            text = category.name,
+                            fontFamily = font,
+                            fontSize = 16.sp,
+                            color = SoftBeige,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
