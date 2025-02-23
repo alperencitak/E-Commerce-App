2f26b23ad9f6265b346258cce381933f23959511
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-24 00:38:56 +0300

    Category service new methods

--- server/app/service/category_service.py
+++ server/app/service/category_service.py
@@ -9,8 +9,13 @@ class CategoryService:
     categories_schema = CategorySchema(many=True)
 
     @classmethod
-    def get_all(cls):
-        categories = db.session.scalars(db.select(Category)).all()
+    def get_all_parent(cls):
+        categories = db.session.scalars(db.select(Category).where(Category.parent_id == None)).all()
+        return cls.categories_schema.dump(categories)
+
+    @classmethod
+    def get_child_by_parent_id(cls, parent_id):
+        categories = db.session.scalars(db.select(Category).where(Category.parent_id == parent_id)).all()
         return cls.categories_schema.dump(categories)
 
     @classmethod
@@ -42,11 +47,13 @@ class CategoryService:
     def update(cls, data):
         category_id = data.category_id
         name = data.name
+        parent_id = data.parent_id
         category = db.session.get(Category, category_id)
         if not category:
             return NotFound(f"Category not found by id: {category_id}")
 
         category.name = name
+        category.parent_id = parent_id
         db.session.commit()
 
         return cls.category_schema.dump(data)
