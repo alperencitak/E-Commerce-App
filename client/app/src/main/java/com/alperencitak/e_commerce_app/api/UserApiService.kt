6c20c606bfe3cbc937a6e957c97c7227c726001f
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-02-27 15:37:05 +0300

    Delete and Update methods added in User api

--- client/app/src/main/java/com/alperencitak/e_commerce_app/api/UserApiService.kt
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/api/UserApiService.kt
@@ -1,7 +1,10 @@
 package com.alperencitak.e_commerce_app.api
 
 import com.alperencitak.e_commerce_app.model.User
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface UserApiService {
@@ -12,6 +15,16 @@ interface UserApiService {
     @GET("user/{id}")
     suspend fun getUserById(
         @Path("id") id: Int
+    ): User
+
+    @DELETE("user/delete/{id}")
+    suspend fun deleteById(
+        @Path("id") id: Int
+    )
+
+    @PUT("user/update")
+    suspend fun update(
+        @Body user: User
     ): User
 
 }
