3ca1944ebe4809e845fb8621213607702cc46864
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-03-02 03:28:29 +0300

    Update AuthViewModel.kt

--- client/app/src/main/java/com/alperencitak/e_commerce_app/viewmodel/AuthViewModel.kt
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/viewmodel/AuthViewModel.kt
@@ -18,10 +18,13 @@ class AuthViewModel @Inject constructor(
 ): ViewModel() {
 
     private val _loginResponse = MutableStateFlow<User?>(null)
-    val loginResponse: StateFlow<User?> get() = _loginResponse
+    val loginResponse: StateFlow<User?> = _loginResponse
 
     private val _registerResponse = MutableStateFlow<User?>(null)
-    val registerResponse: StateFlow<User?> get() = _registerResponse
+    val registerResponse: StateFlow<User?> = _registerResponse
+
+    private val _currentUserId = MutableStateFlow<String?>(null)
+    val currentUserId: StateFlow<String?> = _currentUserId
 
     private val _loading = MutableStateFlow(false)
     val loading: StateFlow<Boolean> = _loading
@@ -30,7 +33,9 @@ class AuthViewModel @Inject constructor(
         viewModelScope.launch {
             try {
                 _loading.value = true
-                _loginResponse.value = authRepository.login(loginRequest)
+                val user = authRepository.login(loginRequest)
+                _loginResponse.value = user
+                _currentUserId.value = user.user_id.toString()
             }catch (e: Exception){
                 e.printStackTrace()
             }finally {
@@ -44,6 +49,32 @@ class AuthViewModel @Inject constructor(
             try {
                 _loading.value = true
                 _registerResponse.value = authRepository.register(registerRequest)
+            }catch (e: Exception){
+                e.printStackTrace()
+            }finally {
+                _loading.value = false
+            }
+        }
+    }
+
+    fun logout(){
+        viewModelScope.launch {
+            try {
+                _loading.value = true
+                authRepository.logout()
+            }catch (e: Exception){
+                e.printStackTrace()
+            }finally {
+                _loading.value = false
+            }
+        }
+    }
+
+    fun getCurrentUserId(){
+        viewModelScope.launch {
+            try {
+                _loading.value = true
+                _currentUserId.value = authRepository.getCurrentUserId()
             }catch (e: Exception){
                 e.printStackTrace()
             }finally {
