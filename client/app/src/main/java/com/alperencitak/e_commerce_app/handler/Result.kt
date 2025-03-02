4c5b1a003748f1fd89ffa46a1f040f519e216635
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-03-02 16:27:53 +0300

    Result model written for exception handling

---
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/handler/Result.kt
@@ -0,0 +1,7 @@
+package com.alperencitak.e_commerce_app.handler
+
+sealed class Result<out T> {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val message: String) : Result<Nothing>()
+    data object Loading : Result<Nothing>()
+}
