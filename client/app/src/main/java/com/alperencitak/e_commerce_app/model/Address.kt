55fe3eebb3da9a66e476b75a5a2dd066332c207c
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-03-05 17:10:20 +0300

    Address Id default value 0

--- client/app/src/main/java/com/alperencitak/e_commerce_app/model/Address.kt
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/model/Address.kt
@@ -1,7 +1,7 @@
 package com.alperencitak.e_commerce_app.model
 
 data class Address(
-    val address_id: Int,
+    val address_id: Int = 0,
     val user_id : Int,
     val address_line1 : String,
     val address_line2 : String,
