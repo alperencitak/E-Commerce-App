03ead34199a8be5321fa8b0e88799e8f9b66bf85
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-03-08 01:15:05 +0300

    Update build.gradle.kts

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation(libs.gson)
+
     implementation(libs.coil.compose)
     implementation(libs.androidx.datastore.preferences)
 
