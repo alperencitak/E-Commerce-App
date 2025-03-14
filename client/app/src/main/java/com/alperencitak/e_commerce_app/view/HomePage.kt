9268044e1fae6307344af99b088754ea1a3cef9a
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-03-14 15:09:32 +0300

    Campaign pager has been set and added new list

--- client/app/src/main/java/com/alperencitak/e_commerce_app/view/HomePage.kt
+++ client/app/src/main/java/com/alperencitak/e_commerce_app/view/HomePage.kt
@@ -2,17 +2,17 @@ package com.alperencitak.e_commerce_app.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -27,11 +27,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -47,20 +48,17 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import coil.compose.rememberAsyncImagePainter
 import com.alperencitak.e_commerce_app.R
 import com.alperencitak.e_commerce_app.ui.theme.DarkPurple
-import com.alperencitak.e_commerce_app.ui.theme.LightCream
-import com.alperencitak.e_commerce_app.ui.theme.LightGray
 import com.alperencitak.e_commerce_app.ui.theme.LightPurple
-import com.alperencitak.e_commerce_app.ui.theme.Purple
-import com.alperencitak.e_commerce_app.ui.theme.SoftBeige
 import com.alperencitak.e_commerce_app.ui.theme.White
 import com.alperencitak.e_commerce_app.utils.SmallProductCard
 import com.alperencitak.e_commerce_app.viewmodel.AddressViewModel
 import com.alperencitak.e_commerce_app.viewmodel.AuthViewModel
 import com.alperencitak.e_commerce_app.viewmodel.ProductViewModel
 import com.alperencitak.e_commerce_app.viewmodel.UserViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomePage(navHostController: NavHostController) {
@@ -71,14 +69,22 @@ fun HomePage(navHostController: NavHostController) {
     val currentUserId = authViewModel.currentUserId.collectAsState()
     val address = addressViewModel.address.collectAsState()
     val productList = productViewModel.productList.collectAsState()
+    val productResponse = productViewModel.productResponse.collectAsState()
     val user = userViewModel.user.collectAsState()
     val font = FontFamily(
         Font(
             R.font.montserrat_bold
         )
     )
+    val campaignImages = listOf(
+        R.drawable.campaign2,
+        R.drawable.campaign3,
+        R.drawable.campaign1,
+        R.drawable.campaign4,
+    )
     authViewModel.getCurrentUserId()
     productViewModel.fetchBestSellers()
+    productViewModel.fetchByCategoryId(6)
     currentUserId.value?.let {
         userViewModel.fetchUserById(it.toInt())
     }
@@ -87,9 +93,10 @@ fun HomePage(navHostController: NavHostController) {
     }
     var addressText = ""
     address.value?.let {
-        if(it.city != null || it.country != null){
-            addressText = "${address.value!!.city}/${address.value!!.country}, ${address.value!!.address_line2}"
-        }else{
+        if (it.city != null || it.country != null) {
+            addressText =
+                "${address.value!!.city}/${address.value!!.country}, ${address.value!!.address_line2}"
+        } else {
             addressText = "No registered address."
         }
     }
@@ -132,7 +139,13 @@ fun HomePage(navHostController: NavHostController) {
                 )
             }
         }
-        ScrollableCard()
+        ScrollableCard(campaignImages)
+        Text(
+            text = "Best Sellers",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 16.dp, bottom = 8.dp, top = 16.dp)
+        )
         LazyRow(
             modifier = Modifier
                 .fillMaxWidth()
@@ -141,6 +154,24 @@ fun HomePage(navHostController: NavHostController) {
         ) {
             items(productList.value) { product ->
                 SmallProductCard(product, navHostController)
+            }
+        }
+        productResponse.value?.let {
+            Text(
+                text = "Laptops",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(White)
+                    .padding(top = 8.dp, bottom = 16.dp)
+            ) {
+                items(it.products) { product ->
+                    SmallProductCard(product, navHostController)
+                }
             }
         }
         Box(
@@ -157,12 +188,7 @@ fun HomePage(navHostController: NavHostController) {
                     top = 32.dp, bottom = 64.dp, start = 32.dp, end = 32.dp
                 ), verticalArrangement = Arrangement.Center
             ) {
-                Text(text = "HOME", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-                Text(
-                    text = "\nLorem Ipsum is simply dummy text of the printing " + "and typesetting industry. Lorem Ipsum has been the " + "industry's standard dummy text ever since the 1500s, when an" + "and typesetting industry. Lorem Ipsum has been the " + "industry's standard dummy text ever since the 1500s, when an" + "and typesetting industry. Lorem Ipsum has been the " + "industry's standard dummy text ever since the 1500s, when an" + "and typesetting industry. Lorem Ipsum has been the " + "industry's standard dummy text ever since the 1500s, when an" + "and typesetting industry. Lorem Ipsum has been the " + "industry's standard dummy text ever since the 1500s, when an",
-                    fontSize = 16.sp,
-                    fontFamily = font
-                )
+                Spacer(modifier = Modifier.height(512.dp))
             }
         }
     }
@@ -170,8 +196,19 @@ fun HomePage(navHostController: NavHostController) {
 
 
 @Composable
-fun ScrollableCard() {
-    val pagerState = rememberPagerState { 4 }
+fun ScrollableCard(campaignImages: List<Int>) {
+    val pagerState = rememberPagerState { campaignImages.size }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        while(true){
+            delay(6000)
+            coroutineScope.launch {
+                val nextPage = (pagerState.currentPage + 1) % campaignImages.size
+                pagerState.animateScrollToPage(nextPage)
+            }
+        }
+    }
 
     Card(
         modifier = Modifier.fillMaxWidth(),
@@ -185,8 +222,8 @@ fun ScrollableCard() {
                     .aspectRatio(16f / 9f)
             ) { page ->
                 Image(
-                    painter = painterResource(R.drawable.test),
-                    contentDescription = "Test $page",
+                    painter = painterResource(campaignImages[page]),
+                    contentDescription = "$page",
                     modifier = Modifier.fillMaxSize(),
                     contentScale = ContentScale.FillBounds
                 )
